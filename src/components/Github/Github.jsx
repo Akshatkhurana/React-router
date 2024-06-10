ae2270ab5476842cd3c7b24f0c9a8bccae881a56
Akshatkhurana <khuranaakshat33@gmail.com>
2024-06-10 23:37:58 +0530

    All changes done

--- src/components/Github/Github.jsx
+++ src/components/Github/Github.jsx
@@ -12,7 +12,7 @@ function Github() {
     },[])
     return(
     <div className='text-center m-100 bg-gray-600 text-white p-4 text-3xl'>Github followers : {data.followers}
-    <img src={data.avatar_url} alt="" />
+    <img src={data.avatar_url} alt="Error loading image" />
     </div>
     )
 }
